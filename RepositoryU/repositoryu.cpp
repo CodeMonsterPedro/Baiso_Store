630e99cf8bec110a42ff42346f9d5f304f89d329
CodeMonsterPedro <maks.frb00@gmail.com>
2018-11-23 18:39:35 +0200

    [backend][repositoryu] add some functions for repositoryu

--- RepositoryU/repositoryu.cpp
+++ RepositoryU/repositoryu.cpp
@@ -1,6 +1,35 @@
-#include "repositoryu.h"
+﻿#include "repositoryu.h"
 
 RepositoryU::RepositoryU(QObject *parent) : QObject(parent)
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setDatabaseName("HUI");
 
+    db.setUserName("HUI");
+    db.setHostName("HUI");
+    db.setPassword("123");
+
+    if(!db.open()){
+        isConnected=false;
+        qDebug()<<db.lastError();
+        //messege about no database conntection
+    }else{
+        isConnected=true;
+        QStringList tables = db.tables();
+        foreach (QString str,tables){
+            qDebug()<<"Table: "<<str;
+        }
+    }
+
+}
+
+QStringList RepositoryU::GetRequest(QString request)
+{
+    QStringList requestResult;
+    return requestResult;
+}
+
+int RepositoryU::SetRequest(QString request)
+{
+    return 0;
 }
--- RepositoryU/repositoryu.h
+++ RepositoryU/repositoryu.h
@@ -1,7 +1,21 @@
-#ifndef REPOSITORYU_H
+﻿#ifndef REPOSITORYU_H
 #define REPOSITORYU_H
 
 #include <QObject>
+
+#include <QtSql>
+#include<QSqlDatabase>
+#include<QSqlQuery>
+#include<QSqlError>
+#include<QSqlField>
+#include<QSqlIndex>
+#include<QSqlRecord>
+
+#include<QDebug>
+
+#include<QStringList>
+#include<QString>
+
 
 class RepositoryU : public QObject
 {
@@ -9,9 +23,16 @@ class RepositoryU : public QObject
 public:
     explicit RepositoryU(QObject *parent = nullptr);
 
+    QStringList GetRequest(QString request);
+    int SetRequest(QString request);
+
+
 signals:
 
 public slots:
+
+private:
+    bool isConnected=false;
 };
 
 #endif // REPOSITORYU_H
